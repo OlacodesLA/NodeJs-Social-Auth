c04f4d595c9f1912948a436eb83a862afe2162e2
OlacodesLA <zaccheausjide@gmail.com>
2023-09-11 19:40:52 +0100

    feat: (Google | Facebook | Github) Auth

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const session = require("express-session");
+const authRouter = require("./src/controllers/google-auth");
+const facebookRouter = require("./src/controllers/facebook-auth");
+const githubRouter = require("./src/controllers/github-auth");
+const protectedRouter = require("./src/controllers/protected-route");
+const passport = require("passport");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+app.set("view engine", "ejs");
+
+const connectToMongoDb = () => {
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("Connected to MongoDb.."))
+    .catch((error) => {
+      console.log("Error in connecting to mongoDB " + error);
+      throw error;
+    });
+};
+connectToMongoDb();
+
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user);
+});
+passport.deserializeUser(function (obj, cb) {
+  cb(null, obj);
+});
+
+app.get("/", (req, res) => {
+  res.render("auth");
+});
+
+app.use("/auth/google", authRouter);
+app.use("/auth/facebook", facebookRouter);
+app.use("/auth/github", githubRouter);
+app.use("/protected", protectedRouter);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("App listening on port " + port));
---
+++ readMe.md
@@ -0,0 +1,15 @@
+## 🚀 Social-Auth V1.0
+
+Using Passport.js library of NodeJS, App includes sign-up / sign-in with Google, Facebook and Github functionality.
+
+## 💼 Prerequisites
+
+- [Node.js](https://nodejs.org/en/download/)
+
+## ⚙️ Installing
+
+1. Clone the latest code on the master branch
+2. Install dependencies
+   - `npm install`
+3. Run the app!
+   - `node index.js`
